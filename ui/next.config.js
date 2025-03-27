95cc698d161edb62d36f99a6b06595901d15eccd
davem1988 <davidgaudreau1988@gmail.com>
2025-03-26 22:20:43 -0400

    fix next.config 3

--- ui/next.config.js
+++ ui/next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
           source: "/:path*",
           headers: [
             { key: "X-Frame-Options", value: "SAMEORIGIN" },
-            { key: "Content-Security-Policy", value: "frame-ancestors 'self' http://136.243.60.24:30110" }
+            { key: "Content-Security-Policy", value: "frame-ancestors 'self' https://136.243.60.24:30110" }
           ]
         }
       ];
