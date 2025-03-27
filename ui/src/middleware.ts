61b6ee642cb89716613ee37becd71cdca7764408
davem1988 <davidgaudreau1988@gmail.com>
2025-03-26 22:39:52 -0400

    change the middleware.ts

--- ui/src/middleware.ts
+++ ui/src/middleware.ts
@@ -5,7 +5,6 @@ import { NextRequest, NextResponse } from "next/server";
 export function middleware(req: NextRequest) {
 	const userAgent = req.headers.get("user-agent");
 
-	if (req.nextUrl.searchParams.get("preview") === "true") return;
 	if (userAgent && !userAgent.includes("CitizenFX")) {
 		return NextResponse.redirect(`${req.url}?preview=true`);
 	}
