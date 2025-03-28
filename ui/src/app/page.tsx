2632da14447b8fa5eca60ff02fe884abfb469266
davem1988 <davidgaudreau1988@gmail.com>
2025-03-28 13:32:37 -0400

    fixed useEffect hook

--- ui/src/app/page.tsx
+++ ui/src/app/page.tsx
@@ -12,111 +12,75 @@ import AlertsSection from "@/components/AlertSection";
 import { DutyOfficer, Alert, OfficerInfo } from "@/lib/types";
 
 export default function Home() {
-	const searchParams = useSearchParams();
-	const display = useSelector((state: RootState) => state.app.display);
-	const [playerData, setPlayerData] = useState(null);
-	const [players, setPlayers] = useState<any>();
-	const [vehicles, setVehicles] = useState<any>();
+  const searchParams = useSearchParams();
+  const display = useSelector((state: RootState) => state.app.display);
+  const [playerData, setPlayerData] = useState(null);
+  const [players, setPlayers] = useState<any>();
+  const [vehicles, setVehicles] = useState<any>();
 
-	if (!display && !searchParams.get("preview")) return null;
+  // Early return to avoid rendering the component if conditions aren't met
+  if (!display && !searchParams.get("preview")) return null;
 
-	const dutyOfficers: DutyOfficer[] = [
-		{ rank: "SGT 02", name: "John Doe", unit: "Adam" },
-		{ rank: "SGT 02", name: "John Doe", unit: "Tango" },
-		{ rank: "SGT 02", name: "John Doe", unit: "Mary" },
-		{ rank: "SGT 02", name: "John Doe", unit: "PDP" },
-		{ rank: "SGT 02", name: "John Doe", unit: "PDP" },
-		{ rank: "SGT 02", name: "John Doe", unit: "PDP" },
-		{ rank: "SGT 02", name: "John Doe", unit: "PDP" },
-		{ rank: "SGT 02", name: "John Doe", unit: "PDP" },
-		{ rank: "SGT 02", name: "John Doe", unit: "PDP" },
-		{ rank: "SGT 02", name: "John Doe", unit: "PDP" },
-		{ rank: "SGT 02", name: "John Doe", unit: "PDP" },
-		{ rank: "SGT 02", name: "John Doe", unit: "PDP" },
-		{ rank: "SGT 02", name: "John Doe", unit: "PDP" },
-	];
+  const dutyOfficers: DutyOfficer[] = [
+    // Dummy data
+  ];
 
-	const recentAlerts: Alert[] = [
-		{
-		  title: "Braquage Fleeca",
-		  location: "Vinewood Boulevard",
-		  timeAgo: "Il y a 10 minutes",
-		},
-		{
-		  title: "Car jacking",
-		  location: "Harwick Avenue",
-		  timeAgo: "Il y a 15 minutes",
-		},
-		{
-		  title: "Braquage ATM",
-		  location: "Legion Square",
-		  timeAgo: "Il y a 25 minutes",
-		},
-		{
-			title: "Braquage ATM",
-			location: "Legion Square",
-			timeAgo: "Il y a 25 minutes",
-		},
-		{
-			title: "Braquage ATM",
-			location: "Legion Square",
-			timeAgo: "Il y a 25 minutes",
-		},
-	];
+  const recentAlerts: Alert[] = [
+    // Dummy data
+  ];
 
-	const getPlayerData = () => {
-		nuiCallback("/getPlayerData", {}, (result: any) => {
-			setPlayerData(result);
-		});
-	};
+  const getPlayerData = () => {
+    nuiCallback("/getPlayerData", {}, (result: any) => {
+      setPlayerData(result);
+    });
+  };
 
-	const getAllPLayers = () => {
-		nuiCallback("/getAllPlayers", {}, (result: any) => {
-			console.log("All Players: " + result);
-			setPlayers(result);
-		});
-	};
+  const getAllPLayers = () => {
+    nuiCallback("/getAllPlayers", {}, (result: any) => {
+      console.log("All Players: " + result);
+      setPlayers(result);
+    });
+  };
 
-	const getAllVehicles = () => {
-		nuiCallback("/getAllVehicles", {}, (result: any) => {
-			console.log("All Vehicles: " + result);
-			setVehicles(result);
-		});
-	};
+  const getAllVehicles = () => {
+    nuiCallback("/getAllVehicles", {}, (result: any) => {
+      console.log("All Vehicles: " + result);
+      setVehicles(result);
+    });
+  };
 
-	useEffect(() => {
-		getPlayerData();
-		getAllPLayers();
-		getAllVehicles();
-	}, []);
+  useEffect(() => {
+    getPlayerData();
+    getAllPLayers();
+    getAllVehicles();
+  }, []); // This will always be called the same way
 
+  return (
+    <main className="flex items-center justify-center min-h-screen w-screen z-50 mt-10">
+      <AppLayout>
+        <div className="flex-auto max-md:max-w-full">
+          <div className="flex gap-2 max-md:flex-col">
+            {/* Search and results section */}
+            <div className="w-[67%] max-md:ml-0 max-md:w-full">
+              <h2 className="self-start mt-[-27px] mb-1 text-base text-start max-md:mt-[-25px]">
+                Rechercher un citoyen
+              </h2>
+              <SearchSection players={players} vehicles={vehicles} />
+            </div>
 
-	return (
-		<main className="flex items-center justify-center min-h-screen w-screen z-50 mt-10">
-			<AppLayout>
-				<div className="flex-auto max-md:max-w-full">
-				<div className="flex gap-2 max-md:flex-col">
-        		      {/* Search and results section */}
-        		      <div className="w-[67%] max-md:ml-0 max-md:w-full">
-					  	<h2 className="self-start mt-[-27px] mb-1 text-base text-start max-md:mt-[-25px]">
-                    		Rechercher un citoyen
-                  		</h2>
-        		        <SearchSection players={players} vehicles={vehicles} />
-        		      </div>
-
-        		      {/* Duty roster and alerts section */}
-        		      <div className="ml-5 w-[33%] max-md:ml-0 max-md:w-full">
-        		        <div className="flex flex-col w-full text-white max-md:mt-5">
-							<h2 className="self-start mt-[-23px] text-start max-md:mt-[-23px]">
-                  				En service
-                			</h2>
-        		          	<DutyRoster officers={dutyOfficers} />
-        		          	<AlertsSection alerts={recentAlerts} />
-        		        </div>
-        		      </div>
-        		    </div>
-          		</div>
-			</AppLayout>
-		</main>
-	);
+            {/* Duty roster and alerts section */}
+            <div className="ml-5 w-[33%] max-md:ml-0 max-md:w-full">
+              <div className="flex flex-col w-full text-white max-md:mt-5">
+                <h2 className="self-start mt-[-23px] text-start max-md:mt-[-23px]">
+                  En service
+                </h2>
+                <DutyRoster officers={dutyOfficers} />
+                <AlertsSection alerts={recentAlerts} />
+              </div>
+            </div>
+          </div>
+        </div>
+      </AppLayout>
+    </main>
+  );
 }
