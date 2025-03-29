b6d7a7833ca6cf44acb287759fe79fce6db713c7
davem1988 <davidgaudreau1988@gmail.com>
2025-03-29 09:36:39 -0400

    add selectedCitizen info

--- fivem/os-mdt/server/server.lua
+++ fivem/os-mdt/server/server.lua
@@ -1,4 +1,13 @@
-RegisterServerEvent(cfg.resourceName..":getPlayerCount")
-AddEventHandler(cfg.resourceName..":getPlayerCount", function()
-    TriggerClientEvent(cfg.resourceName..":getPlayerCount", source, GetNumPlayerIndices())
+RegisterNetEvent(cfg.resourceName..":getAllPlayers")
+AddEventHandler(cfg.resourceName..":getAllPlayers", function()
+    local src = source
+    local players = MySQL.query.await('SELECT * FROM players')
+    TriggerClientEvent(cfg.resourceName..":getAllPlayers", src, players)
+end)
+
+RegisterNetEvent(cfg.resourceName..":getAllVehicles")
+AddEventHandler(cfg.resourceName..":getAllVehicles", function()
+    local src = source
+    local vehicles = MySQL.query.await('SELECT * FROM player_vehicles')
+    TriggerClientEvent(cfg.resourceName..":getAllVehicles", src, vehicles)
 end)
--- ui/src/components/SearchSection.tsx
+++ ui/src/components/SearchSection.tsx
@@ -11,16 +11,9 @@ interface SearchSectionProps {
 
 const SearchSection: React.FC<SearchSectionProps> = ({ initialCitizen, players, vehicles }) => {
   const [searchQuery, setSearchQuery] = useState<string>("");
+  const [selectedCitizen, setSelectedCitizen] = useState<any>(null)
 
-  // Mock citizen data
-  const citizenData: CitizenInfo = initialCitizen || {
-    fullName: "John Doe de la Cruz",
-    birthDate: "01/02/2000",
-    infractions: [{ type: "Braquage" }, { type: "Agression" }, { type: "Agression sur agent" }],
-    vehicles: [{ name: "Sultan RS" }, { name: "Jugular de voyou" }, { name: "Jugular de voyou" }],
-    photoUrl:
-      "https://cdn.builder.io/api/v1/image/assets/TEMP/44a3c9b9b797cb2a1016de92aeb9f8b1f8d86bd9?placeholderIfAbsent=true&apiKey=872927278c6d40e4bb42cad9868a24a5",
-  };
+  
 
   // Filter players and vehicles based on search query
   const filteredPlayers = useMemo(() => {
@@ -68,7 +61,7 @@ const SearchSection: React.FC<SearchSectionProps> = ({ initialCitizen, players, 
                     {filteredPlayers && filteredPlayers.length > 0 && filteredPlayers.map((player: any, index: any) => {
                       const charinfo = JSON.parse(player.charinfo);
                       return (
-                        <div key={index} onClick={() => console.log(`Selected player: ${charinfo.firstname} ${charinfo.lastname}`)}>
+                        <div key={index} onClick={() => setSelectedCitizen(player)}>
                           <h3 className="self-start ml-2.5 pt-1 pb-2.5 cursor-pointer">
                             {charinfo.firstname} {charinfo.lastname}
                           </h3>
@@ -95,13 +88,13 @@ const SearchSection: React.FC<SearchSectionProps> = ({ initialCitizen, players, 
           <div className="flex flex-col w-full max-md:mt-9">
             <div className="flex gap-4">
               <div className="flex flex-col items-start py-3 pr-28 pl-3 rounded-md bg-purple-950 bg-opacity-30 shadow-[0px_4px_4px_rgba(0,0,0,0.25)] max-md:pr-5">
-                <h3 className="text-sm font-bold text-white">{citizenData.fullName}</h3>
-                <p className="mt-3.5 text-sm text-white">Né le {citizenData.birthDate}</p>
+                <h3 className="text-sm font-bold text-white">{selectedCitizen ? `${selectedCitizen.charinfo.firstname} ${selectedCitizen.charinfo.lastname}` : 'No Data'}</h3>
+                <p className="mt-3.5 text-sm text-white">{selectedCitizen ? `Né le ${selectedCitizen.charinfo.birthdate}` : 'No Data'}</p>
               </div>
               <img
-                src={citizenData.photoUrl}
+                src={selectedCitizen && selectedCitizen.photoUrl ? selectedCitizen.photoUrl : 'https://static.vecteezy.com/system/resources/thumbnails/003/780/897/small/silhouette-of-anonymous-man-with-question-mark-in-mugshot-or-police-lineup-background-vector.jpg'}
                 className="object-cover shrink-0 rounded-md aspect-[0.9] shadow-[0px_4px_4px_rgba(0,0,0,0.25)] w-[89px]"
-                alt={`Photo of ${citizenData.fullName}`}
+                alt={`Mugshot`}
               />
             </div>
 
@@ -109,13 +102,13 @@ const SearchSection: React.FC<SearchSectionProps> = ({ initialCitizen, players, 
             <h3 className="self-start mt-4 text-sm text-white">Infractions</h3>
             <div className="flex gap-2 px-2.5 pt-3.5 pb-9 text-sm text-white rounded-md bg-purple-950 bg-opacity-30 shadow-[0px_4px_4px_rgba(0,0,0,0.25)] max-h-[170px]">
               <div className="grow shrink-0 w-fit pr-2 min-h-[140px] overflow-y-auto scroll-smooth">
-                {citizenData.infractions.map((infraction, index) => (
+                {/* {citizenData.infractions.map((infraction, index) => (
                   <div key={`infraction-${index}`} className="relative flex gap-5 justify-between px-2.5 py-5 rounded-md text-white mb-2">
                     <div className="absolute inset-0 rounded-md bg-gradient-to-r from-[#787878] to-[#DEDEDE] opacity-[30%]"></div>
                     <div>{infraction.type}</div>
                     <button className="font-bold text-center cursor-pointer z-20">Consulter</button>
                   </div>
-                ))}
+                ))} */}
               </div>
             </div>
 
@@ -123,13 +116,13 @@ const SearchSection: React.FC<SearchSectionProps> = ({ initialCitizen, players, 
             <h3 className="self-start mt-4 text-sm text-white">Véhicules</h3>
             <div className="flex gap-2 px-2.5 pt-3.5 pb-9 text-sm text-white rounded-md bg-purple-950 bg-opacity-30 shadow-[0px_4px_4px_rgba(0,0,0,0.25)] max-h-[170px]">
               <div className="grow shrink-0 w-fit pr-2 min-h-[140px] overflow-y-auto scroll-smooth">
-                {citizenData.vehicles.map((vehicle, index) => (
+                {/* {citizenData.vehicles.map((vehicle, index) => (
                   <div key={`vehicle-${index}`} className="relative flex gap-5 justify-between px-2.5 py-5 rounded-md text-white mb-2">
                     <div className="absolute inset-0 rounded-md bg-gradient-to-r from-[#787878] to-[#DEDEDE] opacity-[30%]"></div>
                     <div>{vehicle.name}</div>
                     <button className="font-bold text-center cursor-pointer z-20">Consulter</button>
                   </div>
-                ))}
+                ))} */}
               </div>
             </div>
 
