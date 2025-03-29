6daaad1b2440f8c6bf34b89eb9f0fcd5d9d7aba8
davem1988 <davidgaudreau1988@gmail.com>
2025-03-29 11:30:01 -0400

    added console.log for debugging

--- ui/src/app/page.tsx
+++ ui/src/app/page.tsx
@@ -49,6 +49,7 @@ export default function Home() {
   }, []); // This will always be called the same way
 
   useEffect(() => {
+    console.log(players)
     const policeOnDuty = (players || []).filter((player) => {
       if (!player.job) return false; // Ensure job exists
   
